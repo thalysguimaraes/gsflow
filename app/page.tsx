59d29d2d49a525d255bc8149026e420ec26100d5
thalysguimaraes <eu@thalysguimaraes.com>
2024-02-14 19:01:09 -0300

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -18,39 +18,43 @@ import Image from "next/image";
 export default function Page() {
 
     return (
-      <div className="flex items-center justify-center min-h-screen bg-slate-100">
-        <Card className="w-[400px]">
-          <CardHeader>
-            <div className="flex justify-center">
-            <Image 
-          src="/goodslogo.svg" 
-          alt="Next.js" 
-          width={36} 
-          height={36} 
-          className='transition ease-in-out delay-150 hover:-translate-y-1 hover:rotate-180 duration-500 dark:filter dark:invert mb-4 mt-2' 
-          />
-            </div>
-            <CardTitle className="text-center -mb-1">Bem-vindo de volta!</CardTitle>
-            <CardDescription className="text-center">Administre as rádios Algar Telecom</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <form>
-              <div className="grid w-full items-center gap-4 mt-4">
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="name">Seu e-mail</Label>
-                  <Input id="name" placeholder="Use um endereço válido" />
-                </div>
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="name">Sua senha</Label>
-                  <Input id="password" type="password" />
-                </div>
+      <div className="flex items-center justify-center min-h-screen bg-slate-100" style={{height: '100vh'}}>
+
+          <Card className="w-full px-36 py-48" style={{width: '50vw', height: '100vh'}}>
+            <CardHeader>
+              <div className="flex justify-center">
+              <Image 
+            src="/tecnomyl.jpg" 
+            alt="Next.js" 
+            width={169} 
+            height={46} 
+            className='transition ease-in-out delay-150 hover:-translate-y-1 hover:rotate-180 duration-500 dark:filter dark:invert mb-12 mt-2 mix-blend-multiply' 
+            />
               </div>
-            </form>
-          </CardContent>
-          <CardFooter className="flex justify-center mt-2">
-            <Button className="w-full" onClick={() => window.location.href='/stores'}>Fazer login</Button>
-          </CardFooter>
-        </Card>
-      </div>
+              <CardTitle className="text-center text-3xl -mb-1">Faça seu login</CardTitle>
+              <CardDescription className="text-center text-md">Bem-vindo de volta! Preencha seus dados:</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <form>
+                <div className="grid w-full items-center gap-4 mt-4">
+                  <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="name">Seu e-mail</Label>
+                    <Input id="name" placeholder="Use um endereço válido" />
+                  </div>
+                  <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="name">Sua senha</Label>
+                    <Input id="password" type="password" />
+                  </div>
+                </div>
+              </form>
+            </CardContent>
+            <CardFooter className="flex flex-col justify-center mt-2 space-y-2">
+              <Button className="w-full" onClick={() => window.location.href='/home-ctc'}>Fazer login</Button>
+              <Button variant="outline" className="w-full" onClick={() => window.location.href='/stores'}>Esqueci minha senha</Button>
+            </CardFooter>
+          </Card>
+          <div className="w-full bg-cover bg-center" style={{ backgroundImage: "url('/geometricbg.svg')", width: '50vw', height: '100vh' }}></div>
+        </div>
+
     )
   }
